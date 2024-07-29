d7303a2132efa4b93e3c85d2a91274bbb9c0d157
Dallas Parker <dallasjamesparker@gmail.com>
2024-07-29 17:16:13 -0600

    initial

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders practice react aws link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Practice React, AWS, and TypeScript/i);
   expect(linkElement).toBeInTheDocument();
 });
